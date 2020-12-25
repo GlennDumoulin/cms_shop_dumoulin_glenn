1a2a45d6c2ded9975d3938d12607d6cf314bbdaa
pgmgent-glendumo <glendumo@student.arteveldehs.be>
2020-12-25 16:38:47 +0100

    Added sass structure and bootstrap

--- public/wp-content/themes/holideco/functions/files.php
+++ public/wp-content/themes/holideco/functions/files.php
@@ -3,7 +3,7 @@
 function loadFiles()
 {
     wp_enqueue_style('main-styles', get_stylesheet_uri());
-    wp_enqueue_style('roboto-font', '"https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"');
+    wp_enqueue_style('roboto-font', 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
     wp_enqueue_script('main-js', get_theme_file_uri('app.js', null, '1.0', true));
 }
 
