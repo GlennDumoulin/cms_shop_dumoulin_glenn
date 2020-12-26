8dc5c6ff7c8d5f875e0fe521c87602d9dbf873f6
pgmgent-glendumo <glendumo@student.arteveldehs.be>
2020-12-26 22:52:07 +0100

    Work on frontpage & card styles

--- public/wp-content/themes/holideco/front-page.php
+++ public/wp-content/themes/holideco/front-page.php
@@ -6,23 +6,25 @@
     <div class="row">
         <?php
             while (have_posts()) { the_post() ?>
-                <div class="post col-6">
-                    <a href="<?php the_permalink() ?>">
-                        <h2>
-                            <?php the_title() ?>
-                        </h2>
-                    </a>
-                    <?php
-                        if (!has_excerpt()) {
-                            the_excerpt();
-                        } else {
-                            echo '<p>' . wp_trim_words(get_the_content(), 20) . '</p>';
-                        }
-                    ?>
-                    <div>
-                        <em>
-                            Geschreven door: <?php the_author_posts_link() ?>
-                        </em>
+                <div class="col-6">
+                    <div class="card">
+                        <a href="<?php the_permalink(); ?>">
+                            <h2>
+                                <?php the_title() ?>
+                            </h2>
+                            <?php
+                                if (has_excerpt()) {
+                                    the_excerpt();
+                                } else {
+                                    echo '<p>' . wp_trim_words(get_the_content(), 20) . '</p>';
+                                }
+                            ?>
+                        </a>
+                        <div>
+                            <em class="author">
+                                Geschreven door: <?php the_author_posts_link() ?>
+                            </em>
+                        </div>
                     </div>
                 </div>
             <?php }
