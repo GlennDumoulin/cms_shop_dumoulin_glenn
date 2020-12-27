7eb56bc4ae44390cf82b220067a0938e4dc7d765
pgmgent-glendumo <glendumo@student.arteveldehs.be>
2020-12-27 23:14:08 +0100

    work on post detail page

--- public/wp-content/themes/holideco/page.php
+++ public/wp-content/themes/holideco/page.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
 <div class="container">
-<h1>
-        Dit is een page! --> <?php the_title(); ?>
+    <h1>
+        <?php the_title(); ?>
     </h1>
 </div>
 
--- public/wp-content/themes/holideco/single.php
+++ public/wp-content/themes/holideco/single.php
@@ -1,8 +1,43 @@
 <?php get_header(); ?>
 
-<div class="container">
-    <h1>Dit is een single page! --> <?php the_title(); ?></h1>
-    <div><?php the_content(); ?></div>
+<div class="container post">
+    <div class="row post-info">
+        <div class="col-6 post-info--main">
+            <h1>
+                <a href="<?php echo site_url('nieuws'); ?>">Nieuws</a> > <?php the_title(); ?>
+            </h1>
+            <div class="post-info--item">
+                <h4>Autheur</h4>
+                <span><?php the_author_posts_link(); ?></span>
+            </div>
+            <div class="post-info--item">
+                <h4>Categorieën</h4>
+                <?php 
+                    if (!has_category('Uncategorized')) { ?>
+                        <span><?php the_category(', '); ?></span>
+                    <?php }
+                    else { ?>
+                        <span>Geen categorieën</span>
+                    <?php }
+                ?>
+            </div>
+            <div class="post-info--item">
+                <h4>Tags</h4>
+                <?php 
+                    if (has_tag()) {
+                        the_tags('<span>', ', ','</span>');
+                    }
+                    else { ?>
+                        <span>Geen tags</span>
+                    <?php }
+                ?>
+            </div>
+        </div>
+        <div class="col-6 post-info--img">
+            <img src="https://picsum.photos/300/200" />
+        </div>
+    </div>
+    <div class="post-content"><?php the_content(); ?></div>
 </div>
 
 <?php get_footer(); ?>
