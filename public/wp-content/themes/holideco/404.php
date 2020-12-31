a03526013c0922532504fd1e39706ca5abe0ed18
pgmgent-glendumo <glendumo@student.arteveldehs.be>
2020-12-31 14:28:10 +0100

    Added default page & more styles and improved some styles

--- public/wp-content/themes/holideco/404.php
+++ public/wp-content/themes/holideco/404.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div class="container">
-    <h1>Oh oh, dit was niet de bedoeling!</h1>
+    <h1>Ho ho ho, dit was niet de bedoeling!</h1>
     <p>Probeer anders een van de pagina's in onze navigatie of klik op de knop hieronder. Onzo excuses voor het ongemak.</p>
     <a href="<?php echo site_url(); ?>" rel="noreferrer noopener" class="btn">Terug naar onze home</a>
 </div>
--- public/wp-content/themes/holideco/page.php
+++ public/wp-content/themes/holideco/page.php
@@ -1,9 +1,26 @@
 <?php get_header(); ?>
 
-<div class="container">
-    <h1>
-        <?php the_title(); ?>
-    </h1>
+<div class="page--<?php echo strtolower(get_the_title()); ?>">
+    <?php if (has_post_thumbnail()) { ?>
+        <?php get_template_part('components/hero', null, array(
+            'content' => get_the_content(),
+        )); ?>
+
+        <div class="container">
+            <h1>
+                <?php the_title(); ?>
+            </h1>
+        </div>
+    <?php } else { ?>
+        <div class="container">
+            <h1>
+                <?php the_title(); ?>
+            </h1>
+            <div>
+                <?php the_content(); ?>
+            </div>
+        </div>
+    <?php } ?>
 </div>
 
 <?php get_footer(); ?>
