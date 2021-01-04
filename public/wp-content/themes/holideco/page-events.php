b5a757158cbd496b997db9455c66e1c4d4870bc7
pgmgent-glendumo <glendumo@student.arteveldehs.be>
2021-01-05 00:49:59 +0100

    Changed posts_per_page for events

--- public/wp-content/themes/holideco/page-events.php
+++ public/wp-content/themes/holideco/page-events.php
@@ -15,7 +15,7 @@
                     $paged = (get_query_var( 'paged' ) ) ? get_query_var('paged') : 1;
                     $events = new WP_Query(array(
                         'post_type' => 'event',
-                        'posts_per_page' => '1',
+                        'posts_per_page' => '8',
                         'paged' => $paged,
                     ));
 
